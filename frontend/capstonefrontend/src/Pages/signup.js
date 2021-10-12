640807a2a2f54fe4741445b7c21d2c4b5b30453f
Salima Chowdhury <salimachowdhury54@gmail.com>
2021-10-12 12:27:23 +0100

    done register form

--- frontend/capstonefrontend/src/Pages/signup.js
+++ frontend/capstonefrontend/src/Pages/signup.js
@@ -2,18 +2,28 @@ import React from 'react';
 import '../Components/nav.css';
 import Nav from '../Components/nav';
 import food4 from '../Resources/food5.jpg';
+import Signupcontainer from '../Containers/Signupcontainer';
 
 export default function signup() {
     return (
-<div className= "ingredient-container" style={{ backgroundImage: `url(${food4})`, backgroundSize: "cover", height:"100vh", backgroundPosition:"center"}}>
+        <div>  
+                
+                <Signupcontainer />
+            </div>)
+{/* <div className= "ingredient-container" style={{ backgroundImage: `url(${food4})`, backgroundSize: "cover", height:"100vh", backgroundPosition:"center"}}>
+ */}
 
 
+            // <div>
+                
+            //     {/* <Nav/>  */}
+            // </div>
+            
+// </div>
 
-            <div>
-                <Nav/> 
-            </div>
-</div>
+
+    // )
 
 
-    )
 }
+
